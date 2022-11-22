363f0f6593ec7aab828f076b0828643b3c6e38f9
Dihni Hassan <dihnihassan@gmail.com>
2022-11-21 21:02:31 -0500

    Finished middle.js

--- findKey.js
+++ findKey.js
@@ -13,19 +13,18 @@ const findKey = function (object, callback) {
   
     return x;
     }
-}console.log(x);
+// }console.log(x);
 
 };
 
 
 
 
-findKey({
+assertEqual(findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-});
-
+}, x => x.stars === 2);
--- middle.js
+++ middle.js
@@ -19,6 +19,40 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-const middle = function (arr) {
-  let middleElement = 
+const middle = function (array) {
+  // for (let value of array) {
+  //  console.log(value);
+  if(array.length <= 2){
+    return [];
+  }
+  if(array.length % 2 !== 0){
+    const pos = array.length / 2; 
+      const flooredPos = Math.floor(pos)
+      return array[flooredPos];
+    }
+if (array.length % 2 === 0){
+  const pos1 = array.length / 2;
+  const pos2 = pos1 - 1;
+  const value1 = array[pos1];
+  const value2 = array[pos2];
+  return [value2, value1];
 }
+};
+
+// console.log(middle([1]));
+// console.log(middle([1, 2, 3, 4, 5]));
+// console.log(middle([1, 2, 3, 4, 5, 6]));
+
+assertArraysEqual(middle([1, 2, 3, 4, 5]), 3);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+
+// middle([1, 2, 3]);
+// middle([1, 2, 3, 4, 5]);
+
+
+
+
+
+
+// For loop to loop through array 
+// .sort???
