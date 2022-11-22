0a5aee2784de342117ad24c26da5e6cacc8238b0
Dihni Hassan <dihnihassan@gmail.com>
2022-11-22 02:21:33 -0500

    Refactored assertArraysEqual.js

--- tail.js
+++ tail.js
@@ -11,6 +11,8 @@ const tail = function(arr) {
   let newArray = arr.slice(0);
   return newArray;
 };
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// assertEqual(words.length, 3);  
+
+module.exports = tail;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,5 @@
+const eqArrays = require('../eqArrays');
+
+
+
+
